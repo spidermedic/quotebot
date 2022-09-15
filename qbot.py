87ca24410e6268223ef83ac0c7448bbf3c5322cd
spidermedic <dougmartinrn@gmail.com>
2022-09-15 19:09:22 -0400

    cleaned up duplicated in quote list

--- qbot.py
+++ qbot.py
@@ -3,8 +3,8 @@ import quotes
 
 
 def get_quote():
-    r = random.randint(0, 1871)
-    print(f"\n{quotes.quote[r]['quote']}\n-{quotes.quote[r]['by']}\n")
+    r = random.randint(0, len(quotes.quotes) - 1)
+    print(f"\n{quotes.quotes[r]['quote']}\n-{quotes.quotes[r]['by']}\n")
 
 
 if __name__ == "__main__":
